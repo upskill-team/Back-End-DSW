56f009254667639d2cbb3efdbcddd64fa210e9fa
carlex74 <r.icardogugliermino@gmail.com>
2025-08-29 18:40:29 -0300

    refactor: Add Professor schema (#48)
    * refactor: Add Professor schema
    
    * fix(professor): correct schema filename and remove unused imports
    
    ---------
    
    Co-authored-by: NiconiKimg <pedemontenicolas2004@gmail.com>

--- src/shared/middlewares/validate.middleware.ts
+++ src/shared/middlewares/validate.middleware.ts
@@ -2,6 +2,14 @@ import { Request, Response, NextFunction } from 'express';
 import { BaseSchema, safeParse, flatten, BaseIssue } from 'valibot';
 import { HttpResponse } from '../response/http.response.js';
 
+
+/**
+ *  Middleware to validate request bodies against a given Valibot schema.
+ *  If validation passes, the request body is replaced with the parsed output.
+ *  If validation fails, a 400 Bad Request response is sent with the validation errors.
+ * @param schema - Valibot schema to validate the request body against
+ * @returns 
+ */
 export const validationMiddleware = <TInput, TOutput>(
   schema: BaseSchema<TInput, TOutput, BaseIssue<TInput>>
 ) => {
