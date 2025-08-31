7a88c9c86d5788884664d652d660831dcc19e793
Franco Zariaga <francozariaga.zariaga@gmail.com>
2025-08-31 16:06:38 -0300

    refactor(student): Remove POST endpoint /api/students
    Co-authored-by: LucaTvl <LucaTvl@users.noreply.github.com>

--- src/models/student/student.routes.ts
+++ src/models/student/student.routes.ts
@@ -2,14 +2,13 @@ import { Router } from 'express';
 import {
   findAll,
   findOne,
-  add,
   update,
   remove,
 } from './student.controller.js';
 
 import { authMiddleware } from '../../auth/auth.middleware.js';
 import { validationMiddleware } from '../../shared/middlewares/validate.middleware.js';
-import { CreateStudentSchema, UpdateStudentSchema } from './student.schemas.js';
+import { UpdateStudentSchema } from './student.schemas.js';
 
 
 export const studentRouter = Router();
@@ -18,8 +17,6 @@ studentRouter.use(authMiddleware);
 
 studentRouter.get('/', findAll);
 studentRouter.get('/:id', findOne);
-
-studentRouter.post('/', validationMiddleware(CreateStudentSchema), add); //Candidate for removal
 
 studentRouter.put('/:id', validationMiddleware(UpdateStudentSchema), update);
 studentRouter.patch('/:id', validationMiddleware(UpdateStudentSchema), update);
--- src/models/student/student.schemas.ts
+++ src/models/student/student.schemas.ts
@@ -1,16 +1,5 @@
 import * as v from 'valibot';
 
-// Used to validate data when creating a student
-export const CreateStudentSchema = v.object({
-  name: v.pipe(v.string(), v.minLength(1, 'El nombre es requerido.')),
-  surname: v.pipe(v.string(), v.minLength(1, 'El apellido es requerido.')),
-  mail: v.pipe(v.string(), v.email('El formato del email no es válido.')),
-  password_plaintext: v.pipe(
-    v.string(),
-    v.minLength(8, 'La contraseña debe tener al menos 8 caracteres.')
-  ),
-});
-
 // Used to validate data when updating a student
 export const UpdateStudentSchema = v.partial(
   v.object({
@@ -19,5 +8,4 @@ export const UpdateStudentSchema = v.partial(
   })
 );
 
-export type CreateStudentType = v.InferOutput<typeof CreateStudentSchema>;
 export type UpdateStudentType = v.InferOutput<typeof UpdateStudentSchema>;
