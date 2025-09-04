9720bf1d4247b6602add3925291c8d6dd7c68ea5
Franco Zariaga <francozariaga.zariaga@gmail.com>
2025-09-04 17:34:02 -0300

    feat(courses): add endpoint to fetch professor's courses
    Co-authored-by: LucaTvl <LucaTvl@users.noreply.github.com>

--- src/models/course/course.routes.ts
+++ src/models/course/course.routes.ts
@@ -4,7 +4,7 @@
  */
 
 import { Router } from 'express'
-import { findAll, findOne, add, update, remove } from './course.controller.js'
+import { findAll, findOne, add, update, remove, findMyCourses } from './course.controller.js'
 import { validationMiddleware } from '../../shared/middlewares/validate.middleware.js'
 import {
   CreateCourseSchema,
@@ -21,7 +21,9 @@ const professorOnly = roleAuthMiddleware([UserRole.PROFESSOR])
 const professorOrAdmin = roleAuthMiddleware([UserRole.PROFESSOR, UserRole.ADMIN])
 
 courseRouter.get('/', findAll)
+courseRouter.get('/my-courses', professorOnly, findMyCourses)
 courseRouter.get('/:id', findOne)
+
 
 courseRouter.post('/', professorOnly, validationMiddleware(CreateCourseSchema), add)
 courseRouter.put('/:id', professorOnly, validationMiddleware(UpdateCourseSchema), update)
