7d04281589e98c996c79a2c87769c7dd17af26ff
NiconiKimg <pedemontenicolas2004@gmail.com>
2025-09-06 16:05:15 -0300

    feat: integrate question routes in course router

--- src/models/course/course.routes.ts
+++ src/models/course/course.routes.ts
@@ -3,33 +3,92 @@
  * @remarks Defines the API routes for the Course module.
  */
 
-import { Router } from 'express'
-import { findAll, findOne, add, update, remove, findMyCourses } from './course.controller.js'
-import { validationMiddleware } from '../../shared/middlewares/validate.middleware.js'
+import { Router } from 'express';
+import * as courseController from './course.controller.js';
+import * as questionController from '../question/question.controller.js';
+import { validationMiddleware } from '../../shared/middlewares/validate.middleware.js';
+import { CreateCourseSchema, UpdateCourseSchema } from './course.schemas.js';
 import {
-  CreateCourseSchema,
-  UpdateCourseSchema,
-} from './course.schemas.js'
-import { authMiddleware, roleAuthMiddleware } from '../../auth/auth.middleware.js'
-import { UserRole } from '../user/user.entity.js'
-import { uploadCourseImage } from '../../shared/middlewares/file-upload.middleware.js'
+  CreateQuestionSchema,
+  UpdateQuestionSchema,
+} from '../question/question.schemas.js';
+import {
+  authMiddleware,
+  roleAuthMiddleware,
+} from '../../auth/auth.middleware.js';
+import { UserRole } from '../user/user.entity.js';
+import { uploadCourseImage } from '../../shared/middlewares/file-upload.middleware.js';
 
-export const courseRouter = Router()
+export const courseRouter = Router();
 
-courseRouter.use(authMiddleware)
+courseRouter.use(authMiddleware);
 
+const professorOnly = roleAuthMiddleware([UserRole.PROFESSOR]);
+const professorOrAdmin = roleAuthMiddleware([
+  UserRole.PROFESSOR,
+  UserRole.ADMIN,
+]);
 
-const professorOnly = roleAuthMiddleware([UserRole.PROFESSOR])
-const professorOrAdmin = roleAuthMiddleware([UserRole.PROFESSOR, UserRole.ADMIN])
+courseRouter.get('/', courseController.findAll);
+courseRouter.get('/my-courses', professorOnly, courseController.findMyCourses);
+courseRouter.get('/:id', courseController.findOne);
 
+courseRouter.post(
+  '/',
+  professorOnly,
+  uploadCourseImage.single('image'),
+  validationMiddleware(CreateCourseSchema),
+  courseController.add
+);
+courseRouter.put(
+  '/:id',
+  professorOnly,
+  validationMiddleware(UpdateCourseSchema),
+  courseController.update
+);
+courseRouter.patch(
+  '/:id',
+  professorOnly,
+  validationMiddleware(UpdateCourseSchema),
+  courseController.update
+);
 
-courseRouter.get('/', findAll)
-courseRouter.get('/my-courses', professorOnly, findMyCourses)
-courseRouter.get('/:id', findOne)
+courseRouter.delete('/:id', professorOrAdmin, courseController.remove);
 
+// Question routes not nested under courses
+courseRouter.get('/questions/my', professorOnly, questionController.findMyQuestions);
 
-courseRouter.post('/', professorOnly, uploadCourseImage.single('image'), validationMiddleware(CreateCourseSchema), add)
-courseRouter.put('/:id', professorOnly, validationMiddleware(UpdateCourseSchema), update)
-courseRouter.patch('/:id', professorOnly,validationMiddleware(UpdateCourseSchema), update)
-
-courseRouter.delete('/:id', professorOrAdmin, remove)
+// Question routes - nested under courses
+courseRouter.get(
+  '/:courseId/questions',
+  professorOnly,
+  questionController.findByCourse
+);
+courseRouter.post(
+  '/:courseId/questions',
+  professorOnly,
+  validationMiddleware(CreateQuestionSchema),
+  questionController.add
+);
+courseRouter.get(
+  '/:courseId/questions/:id',
+  professorOnly,
+  questionController.findOne
+);
+courseRouter.put(
+  '/:courseId/questions/:id',
+  professorOnly,
+  validationMiddleware(UpdateQuestionSchema),
+  questionController.update
+);
+courseRouter.patch(
+  '/:courseId/questions/:id',
+  professorOnly,
+  validationMiddleware(UpdateQuestionSchema),
+  questionController.update
+);
+courseRouter.delete(
+  '/:courseId/questions/:id',
+  professorOrAdmin,
+  questionController.remove
+);
