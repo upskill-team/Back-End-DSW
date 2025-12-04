9c607169dd552691444d44a80a57fbaeb47281f5
Frasquito3 <132107743+Frasquito3@users.noreply.github.com>
2025-12-04 11:07:57 -0300

    feat: Agregado de endpoint para recuperar apelaciones de usuarios autenticados (#157)
    * feat: add endpoint to retrieve authenticated user appeals
    
    * bugfix: fix vulnerabilities

--- src/models/appeal/appeal.routes.ts
+++ src/models/appeal/appeal.routes.ts
@@ -7,7 +7,7 @@
  */
 
 import {Router} from 'express'
-import { findAll, findOne, add, update, remove } from "./appeal.controller.js"
+import { findAll, findOne, add, update, remove, findMyAppeals } from "./appeal.controller.js"
 import { validationMiddleware } from '../../shared/middlewares/validate.middleware.js'
 import { CreateAppealSchema, UpdateAppealSchema } from './appeal.schemas.js'
 import { authMiddleware, roleAuthMiddleware } from '../../auth/auth.middleware.js'
@@ -20,6 +20,8 @@ appealRouter.use(authMiddleware)
 
 const studentOnly = roleAuthMiddleware([UserRole.STUDENT])
 const adminOnly = roleAuthMiddleware([UserRole.ADMIN])
+
+appealRouter.get('/me', authMiddleware, findMyAppeals) 
 
 appealRouter.get('/', adminOnly, findAll)
 appealRouter.get('/:id', adminOnly, findOne)
