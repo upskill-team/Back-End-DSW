915aef5d0215c37554dbbfbdc1b983b424f38077
NiconiKimg <pedemontenicolas2004@gmail.com>
2025-12-07 23:27:47 -0300

    feat: add enrollement factory and factories index

---
+++ src/shared/testing/factories/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Índice de factories para tests
+ * Exporta todos los factories disponibles
+ */
+export { userFactory } from './user.factory.js';
+export { studentFactory } from './student.factory.js';
+export { professorFactory } from './professor.factory.js';
+export { courseTypeFactory } from './courseType.factory.js';
+export { courseFactory } from './course.factory.js';
+export { enrollementFactory } from './enrollement.factory.js';
